dab1944eb2fc7fea9f31d7b80a640d3025933a6e
adrian.steller.contractor@bbva.com <adrian.steller.contractor@bbva.com>
2021-03-14 16:20:10 +0100

    Fix bugs

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const WebSocket = require('ws');
 const messages = require('./messages');
-const { from } = require('rxjs');
-const { delay } = require('rxjs/operators');
+const { from, of } = require('rxjs');
+const { delay, concatMap } = require('rxjs/operators');
 
-const baseUrl = process.env.BASE_WS_URL || 'ws://localhost:3100/';
-const chargerId = process.env.BASE_WS_URL || 'c1234';
+const baseUrl = process.env.BASE_WS_URL || 'ws://localhost:3100/widgets/';
+const chargerId = process.env.CHARGE_ID || 'c1234';
 const ws = new WebSocket(baseUrl + chargerId);
 
 let subscription = null;
 
 ws.on('open', function open() {
+  console.log(`[${chargerId}] Connected to the server`);
+  
   subscriiption = from(messages[chargerId])
-    .pipe(delay(2000))
-    .subscribe((message) => {
-      console.log(`[${chargerId}] Sending message ${JSON.stringify(message)} to ${baseUrl+chargerId}`);
-      ws.send(JSON.stringify(message), (err) => {
-        console.error(`[${chargerId}] Error sending message ${JSON.stringify(message)} to ${baseUrl+chargerId}: `, err);
-      });
-    })
+    .pipe(
+      concatMap((message) => of(message).pipe(delay(2000)))
+    )
+    .subscribe(
+      (message) => {
+        console.log(`[${chargerId}] Sending message ${JSON.stringify(message)} to ${baseUrl+chargerId}`);
+        ws.send(JSON.stringify(message), (err) => {
+          if (err) {
+            console.error(`[${chargerId}] Error sending message ${JSON.stringify(message)} to ${baseUrl+chargerId}: `, err);
+            ws.terminate();
+          }
+        });
+      }
+    )
 });
 
 ws.on('error', (error) => {
